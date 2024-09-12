1ba85cf0190938a24e10dbca852a5d0b1c88caef
anandhakrishnans123 <career.anandhakrishnans@gmail.com>
2024-09-12 16:06:14 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -11,7 +11,7 @@ st.markdown(
     """
     <style>
     .custom-button {
-        background-color:#2596BE ; /* blue */
+        background-color:#5BB8D3 ; /* blue */
         padding: 12px 24px;
         font-size: 16px;
         margin: 10px;
